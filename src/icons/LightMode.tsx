70bd817105ef6c5d132fabcd1b72ed6866d9ac64
Joakim Jäderberg <15868581+bananashell@users.noreply.github.com>
2024-01-04 12:45:10 +0100

    Fix svg attributes

--- src/icons/LightMode.tsx
+++ src/icons/LightMode.tsx
@@ -1,7 +1,7 @@
 export const LightMode = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    enable-background="new 0 0 24 24"
+    enableBackground="new 0 0 24 24"
     height="24"
     viewBox="0 0 24 24"
     width="24"
