e0349b8914dbcb248a4bd43ff43d31c0ad992bfa
Joakim Jäderberg <15868581+bananashell@users.noreply.github.com>
2024-01-04 22:41:05 +0100

    Add new game button to highscore-page

--- src/app/highscore/page.tsx
+++ src/app/highscore/page.tsx
@@ -1,11 +1,18 @@
+import Link from "next/link";
 import { trpc } from "../_trpc/serverClient";
 import { Highscore } from "./Highscore";
 
 const HighscorePage = async () => {
   const highscores = await trpc.getHighscore({});
   return (
-    <section className="flex w-full h-screen justify-center items-center flex-col">
+    <section className="flex w-full gap-4 h-screen justify-center items-center flex-col">
       <Highscore highscores={highscores} />
+      <Link
+        href={"/"}
+        className="text-xl bg-white dark:bg-gray-800 hover:bg-gray-100 transition-colors hover:dark:bg-gray-700 px-8 py-4 rounded drop-shadow-lg"
+      >
+        Nytt spel
+      </Link>
     </section>
   );
 };
