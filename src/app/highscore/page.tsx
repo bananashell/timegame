89f0b0f663ad8fcd50e3498abf2fcc2ad9bb0b75
Joakim Jäderberg <15868581+bananashell@users.noreply.github.com>
2024-01-12 09:13:29 +0100

    Make sure highscore is always fresh

--- src/app/highscore/page.tsx
+++ src/app/highscore/page.tsx
@@ -2,10 +2,13 @@ import Link from "next/link";
 import { trpc } from "../_trpc/serverClient";
 import { Highscore } from "./Highscore";
 
+export const dynamic = "force-dynamic";
+
 const HighscorePage = async () => {
   const highscores = await trpc.getHighscore({});
   return (
     <section className="flex w-full gap-4 h-screen justify-center items-center flex-col">
+      <div>{new Date().toISOString()}</div>
       <Highscore highscores={highscores} />
       <Link
         href={"/"}
