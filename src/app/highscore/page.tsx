c48bd65a768cc0fbe472315b037002987eb2cbfb
Joakim Jäderberg <15868581+bananashell@users.noreply.github.com>
2024-01-17 12:39:15 +0100

    Add link back from highscore

--- src/app/highscore/page.tsx
+++ src/app/highscore/page.tsx
@@ -6,9 +6,15 @@ export const dynamic = "force-dynamic";
 
 const HighscorePage = async () => {
   const highscoreData = await trpc.getHighscore({});
+
   return (
     <section className="flex w-full gap-4 h-screen justify-center items-center flex-col">
       <Highscore data={highscoreData} />
+      <section className="mt-8 text-3xl text-center dark:drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.6)] flex flex-col">
+        <Link href="/" className="underline">
+          Tillbaka till start
+        </Link>
+      </section>
     </section>
   );
 };
