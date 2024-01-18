3b7846f57c462bc33470e4c7e355a6516e68524c
Joakim Jäderberg <15868581+bananashell@users.noreply.github.com>
2024-01-18 08:52:12 +0100

    Test

--- src/app/highscore/page.tsx
+++ src/app/highscore/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { trpc } from "../_trpc/serverClient";
 import { Highscore } from "./Highscore";
+import { format } from "date-fns";
+import { LastUpdated } from "./LastUpdated";
 
 // export const dynamic = "force-dynamic";
 export const revalidate = 300;
@@ -11,12 +13,11 @@ const HighscorePage = async () => {
   return (
     <section className="flex w-full gap-4 h-screen justify-center items-center flex-col">
       <Highscore data={highscoreData} />
-      <span>Senast uppdaterad: {new Date().toISOString()}</span>
+      <LastUpdated date={new Date()} />
       <section className="mt-8 text-3xl text-center dark:drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.6)] flex flex-col">
         <Link href="/" className="underline">
           Tillbaka till start
         </Link>
-        sp
       </section>
     </section>
   );
