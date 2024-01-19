cbf647a7b95b20d3679603367966e63ea1fb3058
Joakim Jäderberg <15868581+bananashell@users.noreply.github.com>
2024-01-19 18:28:16 +0100

    Prevent empty guesses (#36)

--- src/app/state/actions/guess/useLockGuess.ts
+++ src/app/state/actions/guess/useLockGuess.ts
@@ -1,12 +1,17 @@
 import { useAtom } from "jotai";
-import { stateAtom } from "@/app/state";
+import { stateAtom, currentEventAtom } from "@/app/state";
 
 export const useLockGuess = () => {
   const [state, setState] = useAtom(stateAtom);
+  const [currentEvent] = useAtom(currentEventAtom);
 
   return async () => {
     if (state.mainState != "playing") {
       throw new Error("Must be playing to be able to lock");
+    }
+
+    if (!currentEvent?.guess) {
+      return;
     }
 
     setState({ mainState: "playing", subState: "displaying correct answer" });
