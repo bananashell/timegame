7614e2e34cfcb04d13a93e878c9143f28c959d3f
Joakim Jäderberg <15868581+bananashell@users.noreply.github.com>
2024-01-24 08:30:08 +0100

    Update loading component UI

--- src/app/game/loading.tsx
+++ src/app/game/loading.tsx
@@ -1,3 +1,7 @@
 export default function Loading() {
-  return <div>Loading</div>;
+  return (
+    <div className="flex w-full h-full justify-center items-center text-gray-800">
+      <span className="text-6xl font-50s">Laddar..</span>
+    </div>
+  );
 }
